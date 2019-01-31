14973ea7489cd7b1d20962029c760f5e979f8d9e
sunyikang <sunyikang@gmail.com>
2019-01-31 17:35:10 +0800

    add unit test for enachina api invoke

--- serverless/test.js
+++ serverless/test.js
@@ -1,8 +1,15 @@
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
+describe('Array', function () {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
+            assert.equal([1, 2, 3].indexOf(4), -1);
+        });
+        it('should return 200', function () {
+            var request = require('request');
+            request('http://qa-manage.enachina.com/v1/appversion', function (error, response, body) {
+                console.log(body)
+                assert.equal(response.statusCode, -1);
+            })
+        });
     });
-  });
 });
