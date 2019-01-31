a8a6e45fcd017ef96de9df6eb45bbf5e09436830
sunyikang <sunyikang@gmail.com>
2019-01-31 17:05:28 +0800

    index.js is able to print out the return from enachina url

--- serverless/index.js
+++ serverless/index.js
@@ -11,12 +11,23 @@ var requestTime = function (req, res, next) {
 
 app.use(requestTime)
 
-app.get('/', function (req, res) {
+app.get('/hello', function (req, res) {
   var responseText = 'Hello World!<br>'
   responseText += '<small>Requested at: ' + req.requestTime + '</small>'
   res.send(responseText)
 })
 
-app.listen(3001)
+app.get('/', function (req, res) {
+  var request = require('request');
+  request('http://qa-manage.enachina.com/v1/appversion', function (error, response, body) {
+    console.log(body)
+    res.send({
+      statusCode: response.statusCode,
+      body: JSON.stringify({
+        message: body
+      })
+    });
+  })
+})
 
 module.exports.hello = serverless(app);
