23b3ec2d1fdf0fbea871e5b401f3012f4439e539
sunyikang <sunyikang@gmail.com>
2019-01-31 16:51:56 +0800

    index.js can get current time stamp

--- serverless/index.js
+++ serverless/index.js
@@ -4,8 +4,19 @@ const serverless = require('serverless-http');
 const express = require('express')
 const app = express()
 
+var requestTime = function (req, res, next) {
+  req.requestTime = Date.now()
+  next()
+}
+
+app.use(requestTime)
+
 app.get('/', function (req, res) {
-  res.send('Hello World from index.js file!')
+  var responseText = 'Hello World!<br>'
+  responseText += '<small>Requested at: ' + req.requestTime + '</small>'
+  res.send(responseText)
 })
+
+app.listen(3001)
 
 module.exports.hello = serverless(app);
