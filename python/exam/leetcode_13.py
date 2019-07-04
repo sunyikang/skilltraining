5af5fc31e14b07d23f7e0586edd668056291b3bb
sunyikang <sunyikang@gmail.com>
2019-07-04 11:45:57 +0800

    add leetcode_13 unfinished solution

---
+++ python/exam/leetcode_13.py
@@ -0,0 +1,83 @@
+'''
+13. Roman to Integer
+
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, two is written as II in Roman numeral, just two one's added together. Twelve is written as, XII, which is simply X + II. The number twenty seven is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.
+
+Example 1:
+
+Input: "III"
+Output: 3
+Example 2:
+
+Input: "IV"
+Output: 4
+Example 3:
+
+Input: "IX"
+Output: 9
+Example 4:
+
+Input: "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+Example 5:
+
+Input: "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+'''
+
+def reassign(list):
+  list = [0, 1]
+
+def append(list):
+  list.append(1)
+
+list = [0]
+reassign(list)
+print(list)
+
+append(list)
+print(list)
+
+class Solution:
+    def __init__(self):
+        self.combination={}
+        self.combination['IV'] = 4
+        self.combination['IX'] = 9
+        self.combination['XL'] = 40
+        self.combination['XC'] = 90
+        self.combination['CD'] = 400
+        self.combination['CM'] = 900
+        
+    def kickout_combinations(self, str, num):
+        return 1
+        
+    def romanToInt(self, s: str) -> int:
+        # IV IX XL XC CD CM ==> move these out from string and add their number into final number
+        # calculate left character without considering combination
+        #
+        # dictionary combination["xx"] = number
+        # original_string
+        # final_count
+
+        result = 0
+        self.kickout_combinations(s, result)
+        return 1
