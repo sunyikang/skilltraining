3accb94f612013a2a37cedb2e87ad1f53b861cac
Yikang Sun <yikang_sun@kelaberetiv.sg>
2020-03-01 19:42:55 +0800

    add cppdatatype.cpp

---
+++ cpp/sandbox/cppdatatype.cpp
@@ -0,0 +1,46 @@
+// C++ program to sizes of data types 
+#include<iostream> 
+using namespace std; 
+  
+int main() 
+{ 
+    // 1 byte
+    cout << "Size of bool : " << sizeof(bool)  
+      << " byte" << endl; 
+    cout << "Size of char : " << sizeof(char)  
+      << " byte" << endl; 
+
+    // 2 bytes
+    cout << "Size of short : " << sizeof(short)  
+      << " bytes" << endl; 
+    cout << "Size of short int : " << sizeof(short int)  
+      << " bytes" << endl;
+
+    // 4 bytes
+    cout << "Size of int : " << sizeof(int) 
+      << " bytes" << endl; 
+    cout << "Size of float : " << sizeof(float)  
+       << " bytes" <<endl; 
+    cout << "Size of wchar_t : " << sizeof(wchar_t)  
+       << " bytes" <<endl; 
+    
+    // 8 bytes
+    cout << "Size of long : " << sizeof(long)  
+       << " bytes" << endl; 
+    cout << "Size of long long : " << sizeof(long long)  
+       << " bytes" << endl; 
+    cout << "Size of long int : " << sizeof(long int)  
+       << " bytes" << endl; 
+    cout << "Size of signed long int : " << sizeof(signed long int) 
+       << " bytes" << endl; 
+    cout << "Size of unsigned long int : " << sizeof(unsigned long int)  
+       << " bytes" << endl; 
+    cout << "Size of double : " << sizeof(double)  
+       << " bytes" << endl; 
+
+    // 16 bytes
+    cout << "Size of long double : " << sizeof(long double)  
+       << " bytes" << endl; 
+    
+    return 0; 
+} 
