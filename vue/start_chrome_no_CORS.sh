bac01baea83ac51e254fe65cab6ab6dedf886399
Yikang Sun <yikang_sun@kelaberetiv.sg>
2020-03-10 11:12:50 +0800

    create script to start chrome to allow run html file without CORS error

---
+++ vue/start_chrome_no_CORS.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+# This script is to start chrome to allow run html file without CORS error
+# https://stackoverflow.com/questions/18586921/how-to-launch-html-using-chrome-at-allow-file-access-from-files-mode
+
+"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --allow-file-access-from-files
