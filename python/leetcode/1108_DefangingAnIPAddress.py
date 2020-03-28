05f246956994ce48f8c6e66764560dbc7d66dfa6
Yikang Sun <yikang_sun@kelaberetiv.sg>
2020-03-28 14:49:13 +0800

    add 1108_DefangingAnIPAddress for python

---
+++ python/leetcode/1108_DefangingAnIPAddress.py
@@ -0,0 +1,40 @@
+"""
+Requirement:
+
+    https://leetcode.com/problems/defanging-an-ip-address/
+
+    Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+    A defanged IP address replaces every period "." with "[.]".
+
+    
+
+    Example 1:
+
+    Input: address = "1.1.1.1"
+    Output: "1[.]1[.]1[.]1"
+    Example 2:
+
+    Input: address = "255.100.50.0"
+    Output: "255[.]100[.]50[.]0"
+    
+
+    Constraints:
+
+    The given address is a valid IPv4 address.
+"""
+
+"""
+Solution:
+    
+    Regenerate the string and replace the . by [.]
+*/
+"""
+
+
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        return address.replace('.', '[.]')
+
+
+print(Solution().defangIPaddr("1.1.1.1"))
