2d3062b91376825a15de01f14704e1f1cefd9902
Yikang Sun <yikang_sun@kelaberetiv.sg>
2020-03-29 00:14:00 +0800

    add 155_MinStack.cpp

---
+++ cpp/leetcode/155_MinStack.cpp
@@ -0,0 +1,78 @@
+"""
+Requirement:
+    https://leetcode.com/problems/min-stack/
+
+    Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+    push(x) -- Push element x onto stack.
+    pop() -- Removes the element on top of the stack.
+    top() -- Get the top element.
+    getMin() -- Retrieve the minimum element in the stack.
+    
+
+    Example:
+
+    MinStack minStack = new MinStack();
+    minStack.push(-2);
+    minStack.push(0);
+    minStack.push(-3);
+    minStack.getMin();   --> Returns -3.
+    minStack.pop();
+    minStack.top();      --> Returns 0.
+    minStack.getMin();   --> Returns -2.
+"""
+
+"""
+Solution:
+
+    2 stack. one for data, one for min data.
+"""
+
+
+#include <stack>
+using namespace std;
+
+class MinStack {
+private:
+    stack<int> _stack;
+    stack<int> _minStack;
+        
+public:
+    /** initialize your data structure here. */
+    MinStack() {
+        
+    }
+    
+    void push(int x) {
+        _stack.push(x);
+        
+        if (_minStack.empty()) {
+            _minStack.push(x);
+        } else {
+            int min = x < _minStack.top() ? x : _minStack.top();
+            _minStack.push(min);
+        }
+    }
+    
+    void pop() {
+        _stack.pop();
+        _minStack.pop();
+    }
+    
+    int top() {
+        return _stack.top();
+    }
+    
+    int getMin() {
+        return _minStack.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
