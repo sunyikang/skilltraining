74cc0cc24785e02d681081128a278821cddd8540
Yikang Sun <yikang_sun@kelaberetiv.sg>
2020-03-30 11:47:18 +0800

    fix 15.cpp

--- cpp/exam_virtuos/15.cpp
+++ cpp/exam_virtuos/15.cpp
@@ -1,15 +1,36 @@
+#include <mutex>
 #include <thread>
 #include <iostream>
+using namespace std;
 
 int maxCount = 5;
 int counter = 1;
+mutex m; // Mutex to protect this counter
 
 void even()
 {
+    while (counter < maxCount)
+    {
+        if (counter % 2 == 1)
+        {
+            lock_guard<mutex> locker (m);
+            cout << "Even: " << counter << "\n";
+            counter++;
+        }
+    }
 }
 
 void odd()
 {
+    while (counter <= maxCount)
+    {
+        if (counter % 2 == 1)
+        {
+            lock_guard<mutex> locker (m);
+            cout << "Odd: " << counter << "\n";
+            counter++;
+        }
+    }
 }
 
 int main()
