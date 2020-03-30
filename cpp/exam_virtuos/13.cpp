e2e6d77f8cd709156d9c64292dc54cb1a80dc339
Yikang Sun <yikang_sun@kelaberetiv.sg>
2020-03-30 11:33:18 +0800

    fix 13.cpp

--- cpp/exam_virtuos/13.cpp
+++ cpp/exam_virtuos/13.cpp
@@ -12,10 +12,23 @@ public:
     enum EType { Mouse, Cat };
 
     Pet(Owner& owner, EType type, string name)
-    : m_owner(owner)
+    : m_owner(owner), m_type(type), m_name(name)
     {
-        m_type = type;
-        m_name = name;
+    }
+    
+    string getText()
+    {
+        string text;
+
+        if (m_type == EType::Mouse) {
+            text += "mouse";
+        } else {
+            text += "cat";
+        }
+
+        text += " named ";
+        text += m_name;
+        return text;
     }
 
 private:
@@ -30,7 +43,12 @@ public:
     Owner(string name, Pet::EType petType, string petName) 
     {
         this->m_name = name;
-        this->m_pet = make_shared<Pet>(this, petType, petName);
+        this->m_pet = make_shared<Pet>((*this), petType, petName);
+    }
+
+    void PrettyPrint() const
+    {
+        cout << "My name is " << m_name << " and I own a " << m_pet->getText() << "\n";
     }
 
 private:
@@ -39,16 +57,20 @@ private:
 };
 
 void PrettyPrint(const Owner& owner)
-{}
+{
+    owner.PrettyPrint();
+}
 
 int main()
 {
     vector<Owner> ownerList;
     ownerList.push_back(Owner("Jack", Pet::EType::Mouse, "Jerry"));
     ownerList.push_back(Owner("Jill", Pet::EType::Cat, "Tom"));
+
     for(const auto& owner : ownerList)
     {
         PrettyPrint(owner);
     }
+
     return 0;
 }
